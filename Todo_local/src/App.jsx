1bd859fa66f6211ec71231cba56c1341cb915845
SupLinn <148838687+SupLinn@users.noreply.github.com>
2024-08-19 19:41:58 +0530

    Updated  App.jsx

--- Todo_local/src/App.jsx
+++ Todo_local/src/App.jsx
@@ -50,11 +50,11 @@ function App() {
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
-                        {/* Todo form goes here */} 
+                       
                         <TodoForm />
                     </div>
                     <div className="flex flex-wrap gap-y-3">
-                        {/*Loop and Add TodoItem here */}
+                        
                         {todos.map((todo) => (
                           <div key={todo.id}
                           className='w-full'
