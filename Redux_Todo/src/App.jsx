27953fe10acdb604a2b241825b5e4e5288dd3e36
SupLinn <148838687+SupLinn@users.noreply.github.com>
2024-08-22 15:59:42 +0530

    Update App.jsx

--- Redux_Todo/src/App.jsx
+++ Redux_Todo/src/App.jsx
@@ -1,14 +1,18 @@
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-      <h1>Tutorial for redux toolkit</h1>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
     </>
   )
 }
