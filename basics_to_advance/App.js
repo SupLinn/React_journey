aa82c605a2921a418232e96c8a7d8c89a416816b
Sushil Kumar Yadav <sushilyadukul@gmail.com>
2025-06-06 00:21:29 +0530

    basics of react

---
+++ basics_to_advance/App.js
@@ -0,0 +1,33 @@
+const header = React.createElement(
+    "div",
+    {id : "parent"},
+    [React.createElement(
+        "div",
+        {id: "child1"},
+        [React.createElement(
+            "h1",
+            {},
+            "I'm h1 tag!"
+        ),React.createElement(
+            "h2",
+            {},
+            "I'm h2 tag!"
+        )]
+    ),React.createElement(
+        "div",
+        {id: "child2"},
+        [React.createElement(
+            "h1",
+            {},
+            "I'm h1 tag!"
+        ),React.createElement(
+            "h2",
+            {},
+            "I'm h2 tag!"
+        )]
+    )]
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(header)
