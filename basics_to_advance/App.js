b7927cd25ae53be2ef5f7319603da164de84a354
Sushil Kumar Yadav <sushilyadukul@gmail.com>
2025-06-12 01:09:37 +0530

    project started

--- basics_to_advance/App.js
+++ basics_to_advance/App.js
@@ -1,36 +1,35 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-const header = React.createElement(
-    "div",
-    {id : "parent"},
-    [React.createElement(
-        "div",
-        {id: "child1"},
-        [React.createElement(
-            "h1",
-            {},
-            "I'm h1 tag!"
-        ),React.createElement(
-            "h2",
-            {},
-            "I'm h2 tag!"
-        )]
-    ),React.createElement(
-        "div",
-        {id: "child2"},
-        [React.createElement(
-            "h1",
-            {},
-            "I'm h1 tag!"
-        ),React.createElement(
-            "h2",
-            {},
-            "I'm h2 tag!"
-        )]
-    )]
-)
+const Header = () => {
+    return (
+        // This div is for the logo
+        <div className="header">
+            <div className="logo-container">
+                <img className= "logo" src="https://www.shutterstock.com/shutterstock/photos/1841202208/display_1500/stock-vector-yummy-food-logo-with-funny-smiling-face-mouth-with-a-stuck-out-tongue-delicious-recipe-tasty-1841202208.jpg"/>
+            </div>
+            <div className="menu-bar">
+                <ul>
+                    <li>Menu</li>
+                    <li>Restraunts</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+
+        </div>
+    )
+}
+
+const App = () => {
+    return (
+        <div className="main-container">
+            <Header />
+        </div>
+    )
+    
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(header)
+root.render(<App/>)
